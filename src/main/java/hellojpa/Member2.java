9667e7e643ab1ed2104f8f1dcf743f170b4c0bd7
cruway <astralkaiser89@gmail.com>
2023-03-07 23:18:03 +0900

    entity study

--- src/main/java/hellojpa/Member2.java
+++ src/main/java/hellojpa/Member2.java
@@ -8,14 +8,21 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Scanner;
 
 @Entity
 @Builder
 @Getter @Setter
+/*@SequenceGenerator(
+        name = "MEMBER2_SEQ_GENERATOR",
+        sequenceName = "MEMBER2_SEQ",
+        initialValue = 1, allocationSize = 1)*/
 public class Member2 {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    /*@GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "MEMGER2_SEQ_GENERATOR")*/
     private String id;
     @Column(name = "name", nullable = false)
     private String username;
